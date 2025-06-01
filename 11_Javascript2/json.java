45d533affa2367e4cff544abd0003777ac5174a1
Marie-Elaine <marieelainelopes@outlook.com>
2025-06-01 14:53:57 -0400

    More JS practice with more advance notions

---
+++ 11_Javascript2/json.java
@@ -0,0 +1,23 @@
+// JSON = Javascript object notation 
+// Javascript like code usable with other languages 
+
+// Its basically javascript object, without declaring the object name. Basically a shared data item
+// compatible with Python, PHP, Node JS, Javascript.
+// A standard "object"
+
+// TLDR: A javascript object without a var name.
+{
+    'name' = 'Marie',
+    'favFoods' : ['Pizza', 'Tacos', 'Salmon'],
+    'age' : 31,
+    'children' : [
+        {
+            'name' : 'Puwu',
+            'age' : 3,
+        },
+        {
+            'name' : 'Awiwi',
+            'age' : 1,
+        }
+    ]
+}
